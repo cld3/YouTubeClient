074506c4e23354aebcfecfc11909a4cb3a2016fc
cld3 <cld_@outlook.com>
2018-07-10 15:01:32 +0300

    Добавлен адаптер для RecyclerView (searchScreen)

---
+++ app/src/main/java/com/example/cld/youtueclient/searchScreen/RecyclerAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.cld.youtueclient.searchScreen
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.cld.youtueclient.R
+import com.example.cld.youtueclient.dataLayer.SearchListItem
+import com.squareup.picasso.Picasso
+
+
+class RecyclerAdapter(var items : MutableList<SearchListItem>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.search_item,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = items[position].title
+        holder.publishedAt.text = items[position].publishedAt
+        Picasso.get().load(items[position].imageUrl).into(holder.image)
+    }
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title = view.findViewById<TextView>(R.id.title)
+        val publishedAt = view.findViewById<TextView>(R.id.publishedAt)
+        val image = view.findViewById<ImageView>(R.id.imageView)
+        init {
+
+        }
+    }
+
+}
