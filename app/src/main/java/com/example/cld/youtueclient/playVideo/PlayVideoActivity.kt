c11b6b500311eb231cc93c304134652b0567d903
cld3 <cld_@outlook.com>
2018-07-13 02:35:07 +0300

    Добавленна функция просмотра комментариев

--- app/src/main/java/com/example/cld/youtueclient/playVideo/PlayVideoActivity.kt
+++ app/src/main/java/com/example/cld/youtueclient/playVideo/PlayVideoActivity.kt
@@ -1,25 +1,44 @@
 package com.example.cld.youtueclient.playVideo
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
-import android.widget.Toast
+import com.example.cld.youtueclient.App
 import com.example.cld.youtueclient.R
 import com.example.cld.youtueclient.dataLayer.SearchListItem
 import com.example.cld.youtueclient.dataLayer.YOUTUBE_API_KEY
+import com.example.cld.youtueclient.dataLayer.YouTubeRepository
 import com.google.android.youtube.player.*
 import kotlinx.android.synthetic.main.play_video_activity.*
+import javax.inject.Inject
 
 
 class PlayVideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+
+    @Inject
+    lateinit var youTubeRepository: YouTubeRepository
 
     lateinit var item: SearchListItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.play_video_activity)
+        (application as App).dataComponent.inject(this)
+
         item = intent.getSerializableExtra("item") as SearchListItem
 
-        playerQQ.initialize(YOUTUBE_API_KEY, this)
+        titleVideo.text = item.title
+        description.text = item.description
+        playerYouTube.initialize(YOUTUBE_API_KEY, this)
+
+        commentRecycler.layoutManager = LinearLayoutManager(baseContext)
+
+        youTubeRepository.getComments(item.videoId)
+                .subscribe{
+                    commentRecycler.adapter = CommentRecyclerAdapter()
+                    (commentRecycler.adapter as CommentRecyclerAdapter).items = it.asList().toMutableList()
+                    Log.d("qq",commentRecycler.adapter.toString())
+                }
     }
 
     override fun onInitializationSuccess(provider: YouTubePlayer.Provider, player: YouTubePlayer, wasRestored: Boolean) {
