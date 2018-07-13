aa8979ddbf0d16a3dbefd111a3e70b99f7019a50
cld3 <cld_@outlook.com>
2018-07-14 02:18:02 +0300

    Добавленн bottm navigatin элемент в MainActivity

--- app/src/main/java/com/example/cld/youtueclient/MainActivity.kt
+++ app/src/main/java/com/example/cld/youtueclient/MainActivity.kt
@@ -1,21 +1,40 @@
 package com.example.cld.youtueclient
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.cld.youtueclient.authorization.SignInActivity
-import com.example.cld.youtueclient.playVideo.PlayVideoActivity
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import com.example.cld.youtueclient.accountScreen.AccountFragment
 import com.example.cld.youtueclient.searchScreen.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainContainer,AccountFragment())
+                        .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainContainer,SearchFragment())
+                        .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
         supportFragmentManager.beginTransaction()
                 .replace(R.id.mainContainer,SearchFragment())
                 .commit()
-        startActivity(Intent(baseContext,SignInActivity::class.java))
     }
 }
